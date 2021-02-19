b75d9bbef71dc81c0887d825424bebeee4086eba
Dave <bbec0689@gmail.com>
2021-02-19 14:25:33 -0700

    updated schedma and db files

--- db/database.js
+++ db/database.js
@@ -1,16 +1,16 @@
-
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "test",
-  password: "CZ~Top~9X19",
+    host: "localhost",
+    user: "root",
+    database: "theBase",
+    password: "databasePassword",
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log("connected");
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
 });
 
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,31 @@ CREATE DATABASE employeesDB;
 
 USE employeesDB;
 
-CREATE TABLE employee (
+
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    role_id INT,
-    manager_id INT,
+    departmentName VARCHAR(50),
     PRIMARY KEY (id)
 );
 
+
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50)
-) 
+    title VARCHAR(50) NOT NULL,    
+    salary INT,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE employee (	CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,	    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,	    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,	    last_name VARCHAR(50) NOT NULL,
+    role_id INT,	    role_id INT NOT NULL,
+    manager_id INT,	    manager_id INT,
+    PRIMARY KEY (id)	    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);	);
+
+
